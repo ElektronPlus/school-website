80aa520f88d910db41661885ebb3ad927bccbae6
Jan Szymański <hello.konhi@gmail.com>
2022-06-18 23:04:08 +0000

    fix: :bug: hydration errors due to multiple links

--- apps/frontend/components/Card/card.tsx
+++ apps/frontend/components/Card/card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import NextImage from '../strapiImage';
 import Moment from 'react-moment';
+import 'moment-timezone';
 import styles from './card.module.css';
 import { ArticleEntity } from '../../generated/graphql';
 
@@ -16,19 +17,13 @@ function Card({ article }: { article: ArticleEntity }) {
         <Link passHref href={`/${ARTICLE_PATH}/${article.attributes.slug}`}>
           <a>
             <NextImage image={article.attributes.image} />
-            <span>
-              <Moment format="MMMM Do YYYY, HH:mm">
+              <Moment tz="Poland" format="MMMM Do YYYY, HH:mm">
                 {article.attributes.publishedAt}
               </Moment>
               &nbsp;•&nbsp;
-              <Link
-                passHref
-                href={`/${CATEGORY_PATH}/${article.attributes.category.data.attributes.slug}`}
-              >
-                <a>{article.attributes.category.data.attributes.name}</a>
-              </Link>
-            </span>
+              <span>{article.attributes.category.data.attributes.name}</span>
             <h3>{article.attributes.title}</h3>
+            <p>{article.attributes.content}</p>
           </a>
         </Link>
       </article>
