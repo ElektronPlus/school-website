ed0ce90382e008a35e57c190c15b07c789699f27
Krystian Wybranowski <krystian.wybranowski@gmail.com>
2022-06-23 11:30:05 +0200

    fix śmieszny błąd rekteta

--- apps/frontend/components/Footer/footer.tsx
+++ apps/frontend/components/Footer/footer.tsx
@@ -114,7 +114,7 @@ function SocialButtonsList({ footerData }: { footerData: GetFooterQuery }) {
 function Wave() {
   return (
     <Box maxHeight={250}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFFFFF" fill-opacity="1" d="M0,96L60,112C120,128,240,160,360,176C480,192,600,192,720,181.3C840,171,960,149,1080,144C1200,139,1320,149,1380,154.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#FFFFFF" fillOpacity="1" d="M0,96L60,112C120,128,240,160,360,176C480,192,600,192,720,181.3C840,171,960,149,1080,144C1200,139,1320,149,1380,154.7L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
     </Box>
   );
 }
