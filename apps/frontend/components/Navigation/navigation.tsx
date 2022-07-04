27c68953e20f74831c0bd02ddb59b964840a01f1
konhi <hello.konhi@gmail.com>
2022-07-04 19:07:26 +0200

    feat(frontend): css-in-js for navigation

--- apps/frontend/components/Navigation/navigation.tsx
+++ apps/frontend/components/Navigation/navigation.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
 import Branding from '../Branding/branding';
-import styles from './navigation.module.css';
 import Search from '../search';
+import { css } from '@emotion/react';
 
 function getLinks(props) {
   return (
-    <ul className={styles.menu}>
+    <ul
+      css={css`
+        padding: 0;
+        width: 100%;
+        margin: 0;
+        @media only screen and (min-width: 768px) {
+          display: flex;
+          align-items: center;
+          align-content: space-between;
+          gap: 32px;
+        }
+      `}
+    >
       {props.navigationRes.map((item) => (
-        <li className={styles.menuItem} key={item.uiRouterKey}>
+        <li
+          css={css`
+            max-width: 100%;
+            list-style-type: none;
+            text-align: center;
+          `}
+          key={item.uiRouterKey}
+        >
           <Link passHref href={item.path}>
             <a>{item.title}</a>
           </Link>
@@ -20,16 +39,29 @@ function getLinks(props) {
 
 export default function Navigation(navigationRes) {
   return (
-    <nav className={styles.nav}>
-      <header className={styles.header}>
+    <nav
+      css={css`
+        padding: 16px;
+        @media only screen and (min-width: 768px) {
+          display: flex;
+          align-items: center;
+          gap: 16px;
+        }
+      `}
+    >
+      <header
+        css={css`
+          width: 100%;
+        `}
+      >
         <Link href="/" passHref>
-          <a className={styles.a}>
+          <a>
             <Branding />
           </a>
         </Link>
       </header>
       {getLinks(navigationRes)}
-      <Search/>
+      <Search />
     </nav>
   );
 }
