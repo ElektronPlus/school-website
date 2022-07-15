29ac629815ce6ad7f03e3a196a4871d690080779
konhi <hello.konhi@gmail.com>
2022-07-15 20:10:43 +0200

    fix

--- apps/frontend/components/article/grid.tsx
+++ apps/frontend/components/article/grid.tsx
@@ -43,8 +43,8 @@ function ArticlesGrid({
         >
           <Masonry
             breakpointCols={{
-              default: 1,
-              1000: 2,
+              default: 2,
+              1000: 1,
             }}
             className={styles.articlesGrid}
             columnClassName={styles.articlesColumn}
