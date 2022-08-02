b8712e3ff74f106c1a8d63c91a25b0e5c1e84a6f
konhi <hello.konhi@gmail.com>
2022-08-02 14:21:50 +0200

    feat(front): material symbols

---
+++ apps/frontend/components/utils/materialSymbols.tsx
@@ -0,0 +1,7 @@
+export function MaterialSymbol({name}: {name: string}) {
+    return (
+        <span aria-hidden css={{fontVariationSettings: "'FILL' 0, 'wght' 700, 'GRAD' 0, 'opsz' 48", fontFamily: 'Material Symbols Rounded'}}>
+            {name}
+        </span>
+    )
+}
--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -11,6 +11,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+          />
         </Head>
         <body>
           <Main />
