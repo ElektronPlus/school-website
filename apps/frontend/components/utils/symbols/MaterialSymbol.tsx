c6ceeb7a99f14f49b63c4e971d13a880e4654643
konhi <hello.konhi@gmail.com>
2022-08-05 17:16:20 +0200

    feat(front): make material symbol customizable

--- apps/frontend/components/utils/symbols/MaterialSymbol.tsx
+++ apps/frontend/components/utils/symbols/MaterialSymbol.tsx
@@ -1,14 +1,18 @@
 interface SymbolProps {
   name: string;
+  fill?: boolean;
+  weight?: number;
+  grade?: number;
+  opticalSize?: number
 }
 
 export const MaterialSymbol: React.FunctionComponent<
   SymbolProps & React.HTMLProps<HTMLSpanElement>
-> = ({ children, name, ...props }) => (
+> = ({ children, name, fill = false, weight = 400, grade = 0, opticalSize = 48, ...props }) => (
   <span
     aria-hidden
     css={{
-      fontVariationSettings: "'FILL' 0, 'wght' 700, 'GRAD' 0, 'opsz' 48",
+      fontVariationSettings: `'FILL' ${fill ? 1 : 0}, 'wght' ${weight}, 'GRAD' ${grade}, 'opsz' ${opticalSize}`,
       fontFamily: 'Material Symbols Rounded',
       fontSize: '1.25rem',
     }}
