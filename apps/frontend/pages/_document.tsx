7d603a4e82708c01399dca2aa150acd48764682a
konhi <hello.konhi@gmail.com>
2022-08-05 17:48:24 +0200

    fix(front): missing amp in link

--- apps/frontend/pages/_document.tsx
+++ apps/frontend/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           />
           <link
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200display=swap"
+            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap"
           />
         </Head>
         <body>
