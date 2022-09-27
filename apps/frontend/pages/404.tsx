848ed44ab40666adc6803ad4daedad4df29a1eac
bkmac <bkmac511@gmail.com>
2022-09-27 17:06:53 +0200

    few changes to 404 website

---
+++ apps/frontend/pages/404.tsx
@@ -0,0 +1,33 @@
+import link from 'next/link'
+import styles from '../styles/404.module.css'
+
+export default function Custom404() {
+  return (
+    <div className={styles.wrapper}>
+
+      <h1 className={styles.mainH}><b>404.</b></h1>
+
+      <a className={styles.properText}>Może szukałeś czegoś innego? Sprawdź to:</a>
+
+      <div className={styles.divlist}>
+
+      <ul className={styles.listmenu}>
+        <li>
+          <a className={styles.mainref} href="/">Strona Główna</a>
+        </li>
+
+        <li>
+          <a className={styles.news} href="/news">Aktualności</a>
+        </li>
+
+        <li>
+          <a className={styles.about} href="/about">O nas</a>
+        </li>
+      </ul>
+
+      </div>
+
+    </div>
+
+  )
+}
