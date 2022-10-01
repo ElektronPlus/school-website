f3fee0046f3af6ca1e93cf45aaf7f37c64201b08
bkmac511 <bkmac511@gmail.com>
2022-10-01 18:28:19 +0200

    applied passHref and other recommended changes

---
+++ apps/frontend/pages/404.tsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import styles from './404.module.css'
+
+export default function Custom404() {
+  return (
+    <div className={styles.wrapper}>
+
+      <h1 className={styles.header}><b>404.</b></h1>
+
+      <a className={styles.properText}>Może szukałeś czegoś innego? Sprawdź to:</a>
+
+      <div className={styles.divlist}>
+
+      <ul className={styles.listmenu}>
+        <li>
+          <Link href="/" passHref>
+          <a className={styles.menu_element}>Strona Główna</a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/news" passHref>
+          <a className={styles.menu_element}>Aktualności</a>
+          </Link>
+        </li>
+
+        <li>
+          <Link href="/about" passHref>
+          <a className={styles.menu_element}>O nas</a>
+          </Link>
+        </li>
+      </ul>
+
+      </div>
+
+    </div>
+
+  )
+}
+//
