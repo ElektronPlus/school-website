90d1b8ecce5305f23d1527fe13cb681c4b2689e3
bkmac <bkmac511@gmail.com>
2022-10-14 11:01:36 +0200

    fix(frontend): Fixed previous 404 page errors

--- apps/frontend/pages/404.tsx
+++ apps/frontend/pages/404.tsx
@@ -6,28 +6,25 @@ import {
   GetErrorPageDocument,
 } from 'generated/graphql';
 import client from 'lib/apolloClient';
-import {GetStaticProps} from 'next';
+import { GetStaticProps } from 'next';
 import Link from 'next/link';
-import {Level, H} from 'react-accessible-headings';
+import { Level, H } from 'react-accessible-headings';
 import styles from './404.module.css';
 
 export default function Custom404({
-                                    errorData,
-                                  }: {
+  errorData,
+}: {
   errorData: GetErrorPageQuery;
 }) {
-  const {title, description, links} = errorData.page404.data.attributes;
+  const { title, description, links } = errorData.page404.data.attributes;
   return (
     <Level>
-      <div css={{
-        text-align: center;
-      }}>
-        <H css={{font-size: 4.688rem;
-          border-bottom: 20px;}}>
+      <div css={{textAlign: "center" }}>
+        <H css={{fontSize: "4.688rem", borderBottom: "20px" }}>
           <b>{title}</b>
         </H>
 
-        <a css={{font-size: 1.25rem;}}>{description}</a>
+        <p css={{ fontSize: "1.25rem" }}>{description}</p>
         <div
           dangerouslySetInnerHTML={{
             __html: DOMPurify.sanitize(links),
@@ -38,7 +35,7 @@ export default function Custom404({
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({params}) => {
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const errorData: GetErrorPageQuery = (
     await client.query({
       query: GetErrorPageDocument,
@@ -46,7 +43,7 @@ export const getStaticProps: GetStaticProps = async ({params}) => {
   ).data;
 
   return {
-    props: {errorData},
+    props: { errorData },
     revalidate: 1,
   };
 };
